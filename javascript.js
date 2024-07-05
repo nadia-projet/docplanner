8aa403842a1783cc9f390365555d71fe67b34ca8
nadia <elbouhalinadia@gmail.com>
2024-07-05 21:24:28 +0100

    commit project

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener("click",() => {
+
+    const body = document.body;
+
+    body.classList.toggle('is-menu');
+ 
+ });
